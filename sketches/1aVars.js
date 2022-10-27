f71bb22ad93a0f3eba29614be8725e68ad074904
Scott Fitzgerald <scott@droolcup.com>
2022-10-27 12:17:06 -0400

    Create 1aVars.js

---
+++ sketches/1aVars.js
@@ -0,0 +1,19 @@
+let variable1 = 22;
+let variable2 = "Hello world";
+let variable3 = false;
+let variable4 = [1, 2, 3];
+let variable5;
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  text("variable1 is " + typeof(variable1), 100, 100);
+  text("variable2 is " + typeof(variable2), 100, 150);
+  text("variable3 is " + typeof(variable3), 100, 200);
+  text("variable4 is " + typeof(variable4), 100, 250);
+  text("variable5 is " + typeof(variable5), 100, 300);
+}
